d62b312b78d516f1763b3bd52234db7169780e54
Jerome Thoma <thoma352>
2026-02-07 12:53:03 +0100

    unvisited junctions - first try

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const map = L.map("map").setView([0, 0], 15)
+const map = L.map("map").setView([49.477015, 5.980889], 15)
 
 L.tileLayer(
   "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
   { attribution: "© OpenStreetMap contributors" }
 ).addTo(map)
+
+unvisited_junctions = fetch("unvisited_junctions.geojson")
+L.geoJSON(unvisited_junctions, {
+  pointToLayer: (feature, latlng) => L.circleMarker(latlng, { color: 'red', radius: 3 })
+}).addTo(map);
 
 const marker = L.circleMarker([0, 0], {
   radius: 8,
@@ -15,7 +20,7 @@ navigator.geolocation.watchPosition(
   pos => {
     const { latitude, longitude } = pos.coords
     marker.setLatLng([latitude, longitude])
-    map.setView([latitude, longitude], 16)
+    map.setView([latitude, longitude], 18)
   },
   err => alert(err.message),
   { enableHighAccuracy: true }

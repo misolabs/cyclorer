5e186bd2e0dd04f305941b67741e18315a8a0e0e
Jerome Thoma <thoma352>
2026-02-10 14:15:07 +0100

    refactoring

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+import "./helpers.js"
+
 const buildTime = "__BUILD_TIME__"
+const homeGPS = [49.497373, 5.978007]
+const nodesGrid = new Map();
+
+// For heading direction
+const MIN_SPEED = 1.0
+
 document.getElementById("buildTime").textContent = buildTime
 
-const homeGPS = [49.497373, 5.978007]
 
 async function loadStats(url) {
   try {
@@ -28,10 +35,10 @@ async function loadJunctions(url) {
     // Build spatial grid index for nodes
     for (const node of geojsonData.features) {
       const key = cellKey(node.geometry.coordinates[1], node.geometry.coordinates[0]);
-      if (!grid.has(key)) grid.set(key, []);
-        grid.get(key).push(node);
+      if (!nodesGrid.has(key)) nodesGrid.set(key, []);
+        nodesGrid.get(key).push(node);
     }
-    console.log("buckets", grid.size)
+    console.log("buckets", nodesGrid.size)
   } catch (err) {
     console.error("Failed to load GeoJSON:", err);
   }
@@ -56,31 +63,7 @@ async function loadEdges(url) {
   }
 }
 
-function computeBearing(lat1, lon1, lat2, lon2) {
-  const toRad = (d) => d * Math.PI / 180;
-  const toDeg = (r) => r * 180 / Math.PI;
 
-  const φ1 = toRad(lat1);
-  const φ2 = toRad(lat2);
-  const Δλ = toRad(lon2 - lon1);
-
-  const y = Math.sin(Δλ) * Math.cos(φ2);
-  const x =
-    Math.cos(φ1) * Math.sin(φ2) -
-    Math.sin(φ1) * Math.cos(φ2) * Math.cos(Δλ);
-
-  return (toDeg(Math.atan2(y, x)) + 360) % 360;
-}
-
-function smoothHeading(headings) {
-  let x = 0, y = 0;
-  for (const h of headings) {
-    const r = h * Math.PI / 180;
-    x += Math.cos(r);
-    y += Math.sin(r);
-  }
-  return (Math.atan2(y, x) * 180 / Math.PI + 360) % 360;
-}
 
 function rotateMap(deg) {
   const mapEl = document.getElementById("map");
@@ -93,58 +76,9 @@ function setStats(total_length, area_count){
   document.getElementById("area_count").textContent = `In ${area_count} areas`
 }
 
-const CELL_SIZE = 0.002; // ≈ 200m in lat/lon (rough)
 
-function cellKey(lat, lon) {
-  const x = Math.floor(lon / CELL_SIZE);
-  const y = Math.floor(lat / CELL_SIZE);
-  return `${x},${y}`;
-}
 
-function nearbyNodes(lat, lon) {
-  const x = Math.floor(lon / CELL_SIZE);
-  const y = Math.floor(lat / CELL_SIZE);
 
-  const result = [];
-
-  for (let dx = -1; dx <= 1; dx++) {
-    for (let dy = -1; dy <= 1; dy++) {
-      const key = `${x + dx},${y + dy}`;
-      if (grid.has(key)) {
-        result.push(...grid.get(key));
-      }
-    }
-  }
-  return result;
-}
-
-Math.toRad = function(deg) {
-  return deg * Math.PI / 180;
-};
-
-function approxDist2(lat1, lon1, lat2, lon2) {
-  const dLat = lat2 - lat1;
-  const dLon = (lon2 - lon1) * Math.cos(lat1 * Math.PI / 180);
-  return dLat*dLat + dLon*dLon;
-}
-
-function haversineDist(lat1, lon1, lat2, lon2){
-  const R = 6371000.0
-
-  const phi1 = Math.toRad(lat1)
-  const phi2 = Math.toRad(lat2)
-  const dphi = Math.toRad(lat2 - lat1)
-  const dlambda = Math.toRad(lon2 - lon1)
-  
-  const a = Math.min(1, Math.sin(dphi/2)**2 + Math.cos(phi1)*Math.cos(phi2)* Math.sin(dlambda/2)**2)
-
-  return 2*R*Math.asin(Math.sqrt(a))
-}
-
-const grid = new Map();
-
-// For heading direction
-const MIN_SPEED = 1.0
 
 // Initialise map
 const map = L.map("map").setView([49.477015, 5.980889], 17)
@@ -228,7 +162,7 @@ button.addEventListener("click", () => {
             // Find closest boundary point
             try{
               const distEl = document.getElementById("candidate-dist")
-              const closeNodes = nearbyNodes(latitude, longitude)
+              const closeNodes = nearbyNodes(nodesGrid, latitude, longitude)
               //distEl.textContent=`${closeNodes.length}`
               let minDist = Infinity
               let closestNode = null

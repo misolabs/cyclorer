45affe769fdc0fe4032432a9b5eb292246186301
Jerome Thoma <thoma352>
2026-02-17 14:12:12 +0100

    refactoring heading direction

--- js/heading.js
+++ js/heading.js
@@ -3,33 +3,35 @@ const MIN_SPEED = 1.0
 const MAX_HISTORY = 5;
 
 let headingHistory = [];
-let stableHeading = null;
 let lastPos = null
 
+// Exports with namespace
 export const Heading = {
+    update: update
+}
 
-    computeBearing(lat1, lon1, lat2, lon2) {
-        const toRad = (d) => d * Math.PI / 180;
-        const toDeg = (r) => r * 180 / Math.PI;
+function computeHeading(lat1, lon1, lat2, lon2) {
+    const toRad = (d) => d * Math.PI / 180;
+    const toDeg = (r) => r * 180 / Math.PI;
 
-        const φ1 = toRad(lat1);
-        const φ2 = toRad(lat2);
-        const Δλ = toRad(lon2 - lon1);
+    const φ1 = toRad(lat1);
+    const φ2 = toRad(lat2);
+    const Δλ = toRad(lon2 - lon1);
 
-        const y = Math.sin(Δλ) * Math.cos(φ2);
-        const x =
-            Math.cos(φ1) * Math.sin(φ2) -
-            Math.sin(φ1) * Math.cos(φ2) * Math.cos(Δλ);
+    const y = Math.sin(Δλ) * Math.cos(φ2);
+    const x =
+        Math.cos(φ1) * Math.sin(φ2) -
+        Math.sin(φ1) * Math.cos(φ2) * Math.cos(Δλ);
 
-        const deg = (toDeg(Math.atan2(y, x)) + 360) % 360;
+    const deg = (toDeg(Math.atan2(y, x)) + 360) % 360;
 
-        // Quantise to 6 opr 12 main directions for more stability
-        const quantised = Math.floor(deg / 60) * 60 + 30
+    // Quantise to 6 opr 12 main directions for more stability
+    const quantised = Math.floor(deg / 60) * 60 + 30
 
-        return quantised
-    },
+    return quantised
+}
 
-    smoothHeadingMode(headings){
+function modeFilter(headings){
     const buckets = new Map()
 
     // Fill buckets
@@ -49,27 +51,27 @@ export const Heading = {
         }
     }
     return heading
-    },
-
-    update(latitude, longitude, speed){
-        // Heading
-        if (lastPos && speed !== null && speed > MIN_SPEED) {
-            const h = Heading.computeBearing(
-                lastPos.latitude,
-                lastPos.longitude,
-                latitude,
-                longitude
-            );
-
-            headingHistory.push(h);
-            if (headingHistory.length > MAX_HISTORY) {
-                headingHistory.shift();
-            }
-        }
-
-        stableHeading = Heading.smoothHeadingMode(headingHistory);
-        lastPos = { latitude, longitude };  
-        
-        return stableHeading
-    }
 }
+
+function update(latitude, longitude, speed){
+    // Heading
+    if (lastPos && speed !== null && speed > MIN_SPEED) {
+        const h = computeHeading(
+            lastPos.latitude,
+            lastPos.longitude,
+            latitude,
+            longitude
+        );
+
+        headingHistory.push(h);
+        if (headingHistory.length > MAX_HISTORY) {
+            headingHistory.shift();
+        }
+    }
+
+    const stableHeading = modeFilter(headingHistory);
+    lastPos = { latitude, longitude };  
+    
+    return stableHeading
+}
+
